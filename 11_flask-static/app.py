7d5486c88cb597509ea185b6fb7b90a78b09bd5f
stanleyhoo1 <stanleyhoo0427@gmail.com>
2024-09-26 02:14:06 -0400

    done

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -8,14 +8,14 @@
 # DEMO
 # basics of /static folder
 import random
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     print("the __name__ of this module is... ")
-#     print(__name__)
-#     return "No hablo queso!"
+@app.route("/")
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return serve_fixie()
 
 
 @app.route("/static/foo.html")
@@ -23,6 +23,12 @@ def h():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
+
+@app.route("/")
+def serve_fixie():
+    print("The __name__ of this module is...")
+    print(__name__)
+    return app.send_static_file('fixie.html')
 
 
 if __name__ == "__main__":  # true if this file NOT imported
