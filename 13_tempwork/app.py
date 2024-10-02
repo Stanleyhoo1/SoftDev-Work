6c625c73e6dd5592c293bff742d76f5d9ef3b63d
stanleyhoo1 <stanleyhoo0427@gmail.com>
2024-10-02 17:22:15 -0400

    finished

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -5,7 +5,7 @@
 # 2024-09-30
 # time spent: 0.5
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)    
 
 import csv
@@ -18,7 +18,8 @@ def read_csv(csvfile):
         dic = {}
         for row in content:
             percent = round(float(row[1]), 1)
-            dic[row[0]] = percent
+            link=row[2]
+            dic[row[0]] = [percent, link]
 
         dic.popitem()
         return dic
@@ -26,7 +27,7 @@ def read_csv(csvfile):
 def choose_random(csvfile):
     data = read_csv(csvfile)
     jobs = [key for key in data.keys()]
-    percents = [value for value in data.values()]
+    percents = [value[0] for value in data.values()]
     percents.sort()
     return ReturnRandom(jobs, percents), data
         
@@ -51,7 +52,7 @@ html_website = '''
     </body>
 </html>'''
 
-def make_table(lists):
+def make_table(lists, data):
     #html table shell
     html_table = '''
         <table class="table" border=1>
@@ -62,11 +63,11 @@ def make_table(lists):
                 _TBODY_
             </tbody>
         </table>'''
-    html_table = html_table.replace("_THEAD_", "<th>Occupations</th>")
+    html_table = html_table.replace("_THEAD_", "<th>Occupations</th><th>Percent</th>")
     tbody = ""
     #using the stats shell, it replaces each of them with the pokemon's actual stats, also if there is no 2nd type it puts none
     for i in lists:
-        tbody += "<tr><td>" + i + "</td></tr>\n\t"
+        tbody += f'<tr><td><a href={data[i][1]}>{i}</a></td><td>{data[i][0]}</td></tr>\n\t'
     html_table = html_table.replace("_TBODY_", tbody[:-2])
     return html_table
 
@@ -78,16 +79,18 @@ def htmlOut(template):
     body += "<p>Period 4</p>\n"
     random_job, data = choose_random('data/occupations.csv')
     body += f"<p>{random_job}</p>\n"
-    body += make_table([key for key in data.keys()])
+    body += make_table([key for key in data.keys()], data)
     template = template.replace("BODY", body)
+    html_file = open("templates/tablified.html", "w")
+    print(template, file=html_file)
+    html_file.close()
     return template    
 
-@app.route("/")                         
+@app.route("/wdywtbwygp")                         
 def hello_world():
     print(__name__)                  
-    a = htmlOut(html_website)
-    print(a)
-    return a
+    htmlOut(html_website)
+    return render_template('tablified.html')
 
 if __name__ == "__main__":
     app.debug = True
