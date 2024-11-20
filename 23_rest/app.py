5e27c216d60972754379b5319c6fedc6c08b8176
Stanley Hoo <shoo50@stuy.edu>
2024-11-20 11:01:42 -0500

    files

---
+++ 23_rest/app.py
@@ -0,0 +1,9 @@
+# Stanley Hoo, Alex Luo
+# 
+# SoftDev
+# K23: A RESTful Journey Skyward
+# 2024-11-20
+# Time spent: 1
+
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+import os, urllib, json
